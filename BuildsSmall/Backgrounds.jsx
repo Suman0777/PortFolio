7568ab52451b53f6c66af5490e24cb25647b1843
suman basak <gorangobasakpkr@gmail.com>
2026-02-23 03:00:30 +0530

    LCP Changes fro web performance

--- BuildsSmall/Backgrounds.jsx
+++ BuildsSmall/Backgrounds.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Backgrounds = ({ children }) => {
   return (
     <div className="relative min-h-screen">
       
-      <div
-        className="fixed inset-0 -z-10 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url('/backgroundimg.jpg')" }}
+      <img
+        src="/backgroundimg.jpg"
+        alt="background"
+        className="fixed inset-0 -z-10 w-full h-full object-cover"
+        loading="eager"
       />
 
       {children}
-
     </div>
-  )
-}
+  );
+};
 
-export default Backgrounds
+export default Backgrounds;
